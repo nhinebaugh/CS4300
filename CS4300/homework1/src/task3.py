abceda2b0a367e24b8c8029b6d75fc9a62a2429b
Nhinebaugh <Nhinebaugh1@gmail.com>
2026-09-23 06:02:04 +0000

    fixed task3

--- CS4300/homework1/src/task3.py
+++ CS4300/homework1/src/task3.py
@@ -1,38 +1,44 @@
-number = -7
+def check_num(number):
+    #return if num is pos neg or 0
+    if number > 0:
+        return "Positive"
+    elif number < 0:
+        return "Negative"
+    else:
+        return "Zero"
 
-if number > 0:
-    num_result = "Pos"
-elif number < 0:
-    num_result = "Neg"
-else:
-    num_result = "Zero"
+def first_ten_primes():
+    #returns the first 10 primes
+    prime_numbers = []
 
-print(number, "is", num_result)
+    for num in range(2, 30):
+        is_prime = True
 
-prime_numbers = []
+        for divisor in range(2, num):
+            if num % divisor == 0:
+                is_prime = False
+                break
+    
+        if is_prime:
+            prime_numbers.append(num)
 
-for num in range(2, 30):
-    is_prime = True
-
-    for divisor in range(2, num):
-        if num % divisor == 0:
-            is_prime = False
+        if len(prime_numbers) == 10:
             break
-    
-    if is_prime:
-        prime_numbers.append(num)
 
-    if len(prime_numbers) == 10:
-        break
+    return prime_numbers
 
-print("First 10 prime num: ", prime_numbers)
+def sum_one_to_one_hundred():
+    #returns the sum of all numbers 1 to 100
+    total = 0
+    current_num = 1
 
-total = 0
-current_num = 1
+    while current_num <= 100:
+        total += current_num
+        current_num += 1
 
-while current_num <= 100:
-    total += current_num
-    current_num += 1
+    return total
 
-print("Sum of number 1 through 100: ", total)
-    
+number = int(input("Enter a Number: "))
+print(number, "is", check_num(number))
+print("First 10 prime numbers: ", first_ten_primes())
+print("Sum of 1 to  100: ", sum_one_to_one_hundred())    
--- CS4300/homework1/tests/test_task3.py
+++ CS4300/homework1/tests/test_task3.py
@@ -1,14 +1,36 @@
 import runpy
+import pytest
 
-def test_if_state():
-    result = runpy.run_path("src/task3.py")
-    assert result ["number"] == -7
-    assert result ["num_result"] == "Neg"
+@pytest.mark.parametrize(
+    "user_input, expected",
+    [
+        ("5", "Positive"),
+        ("-5", "Negative"),
+        ("0", "Zero"),
+    ],
+)
 
-def test_for_loop():
-    result = runpy.run_path("src/task3.py")
-    assert result["prime_numbers"] == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+def test_num_check(monkeypatch, user_input, expected):
+    monkeypatch.setattr("builtins.input", lambda _: user_input)
 
-def test_while_loop():
     result = runpy.run_path("src/task3.py")
-    assert result["total"] == 5050
+    check_num = result["check_num"]
+    number = result["number"]
+
+    assert check_num(number) == expected
+
+def test_prime(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "5")
+
+    result = runpy.run_path("src/task3.py")
+    first_ten_primes = result["first_ten_primes"]
+
+    assert first_ten_primes() == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+def test_sum(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "5")
+
+    result = runpy.run_path("src/task3.py")
+    sum_one_to_one_hundred = result["sum_one_to_one_hundred"]
+
+    assert sum_one_to_one_hundred() == 5050
